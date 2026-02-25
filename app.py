1c171ea77bf67e42ded25ebe9257dbe88193a523
Hector Requena <requenamacayah@gmail.com>
2026-02-25 19:48:21 +0100

    Fix stable port binding configuration

--- app.py
+++ app.py
@@ -300,5 +300,8 @@ def logout():
     return redirect(url_for("login"))
 
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
